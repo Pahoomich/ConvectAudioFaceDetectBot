0d340451f242a3288f2931f153620c696a1078b1
pahoomich <moshguuy@gmail.com>
2020-02-10 15:31:49 +0300

    Add response to msg

--- BotMain.py
+++ BotMain.py
@@ -1,1 +1,25 @@
 import telebot
+
+secret_file = open('token.txt', 'r')
+token = secret_file.read()
+# print(token)
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+bot.polling()
+
+secret_file.close()
