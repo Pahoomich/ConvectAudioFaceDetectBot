9f860c7c2cb1b700304498e95c5d74b5d7798c20
pahoomich <moshguuy@gmail.com>
2020-02-11 01:29:52 +0300

    Add db repository

--- BotMain.py
+++ BotMain.py
@@ -2,6 +2,8 @@ import telebot
 import config
 from pydub import AudioSegment
 from io import BytesIO
+import os
+from DBRepository import DBRepository
 
 bot = telebot.TeleBot(config.token)
 
@@ -19,26 +21,41 @@ def start_massege(message):
 def audio_message_save(massage):
     user_id = massage.from_user.id
     user_name = massage.from_user.username
-    num_audio = 0
+
+    db = DBRepository()
+    # check user in db
+    if (db.find_user_by_uid(user_id)):
+        # if user exist check last num of his voice and add new
+        num_audio = db.count_voice_rows_by_id(user_id) + 1
+    else:
+        num_audio = 0
+        # if user not exist add new user on db
+        db.save_new_user((user_id, user_name))
+        os.mkdir(os.path.join(config.data_path, str(user_id)))
 
     # get voice msg id and info
     voice_id = massage.voice.file_id
     voice_file_info = bot.get_file(voice_id)
+
     # download voice by bytes
     download_file = bot.download_file(voice_file_info.file_path)
+
     # read bytes
     voice_bytes = BytesIO(download_file)
     # create audiosegment and change frame to 16000
     voice_file = AudioSegment.from_file(voice_bytes).set_frame_rate(16000)
+
     # save voice massage in wav format file
-    voice_file.export('{}_message_{}.wav'.format(user_id, num_audio),
+    voice_out_path = os.path.join(str(user_id)
+                                  , 'audio_message_{}.wav'.format(num_audio))
+    voice_file.export(os.path.join(config.data_path, voice_out_path),
                       format='wav',
                       tags={'uid': '{}'.format(user_id),
                             'username': '{}'.format(user_name)})
+    # save voice info to db
+    db.save_voice((voice_out_path, user_id))
 
-    # we have the file in bytes and we can save its to disk
-    # with open('{}_{}.oga'.format(user_id, num_audio), 'wb') as new_voice_file:
-    #     new_voice_file.write(download_file)
+    db.close()
 
 
 if __name__ == '__main__':
---
+++ DBRepository.py
@@ -0,0 +1,48 @@
+import psycopg2
+import config
+
+
+class DBRepository:
+
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect(host="localhost",
+                                               database=config.db_name,
+                                               user=config.db_user_name,
+                                               password=config.db_user_password)
+            self.cursor = self.connection.cursor()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def find_voice_all(self):
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM voices").fetchall()
+
+    def find_user_by_uid(self, uid):
+        select_query = "SELECT * FROM users WHERE users.uid = %s"
+        with self.connection:
+            self.cursor.execute(select_query, (uid,))
+            return self.cursor.fetchone() is not None
+
+    def count_voice_rows_by_id(self, uid):
+        select_query = "SELECT * FROM voices WHERE voices.user_id = %s"
+        with self.connection:
+            self.cursor.execute(select_query, (uid,))
+            return len(self.cursor.fetchall()) is not None
+
+    def save_voice(self, record_to_insert):
+        insert_query = "INSERT INTO voices (path_to_voice_message, user_id) VALUES (%s,%s)"
+        with self.connection:
+            self.cursor.execute(insert_query, record_to_insert)
+            self.connection.commit()
+
+    def save_new_user(self, record_to_insert):
+        insert_query = " INSERT INTO users (uid, username) VALUES (%s,%s)"
+        with self.connection:
+            self.cursor.execute(insert_query, record_to_insert)
+            self.connection.commit()
+
+    def close(self):
+        if (self.connection):
+            self.cursor.close()
+            self.connection.close()
